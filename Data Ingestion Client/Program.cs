9dd155e0aaedbff7de5d0b504673793966d2d0a3
qnetjoe <qnetjoe@users.noreply.github.com>
2022-04-06 15:49:47 -0600

    Basic Hello World Working

---
+++ Data Ingestion Client/Program.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="Program.cs" company="Joe Daily">
+// Copyright (c) Joe Daily. All rights reserved.
+// </copyright>
+
+namespace Ingestion.Client
+{
+    using System;
+
+    /// <summary>
+    /// Data Ingestion Client - Console.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Data Ingestion Client - Console.
+        /// </summary>
+        /// <param name="args"> Command Line Arguments.</param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
